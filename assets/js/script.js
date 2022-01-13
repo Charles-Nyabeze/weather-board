2ce47ba384997e720752d1cf6d986cbd3a4085a5
Charles Nyabeze <charlesnnyabeze@gmail.com>
2022-01-13 15:56:31 -0500

    base styling finished + begining of js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var cities = [];
+
+var cityFormEl=document.querySelector("#search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#currentForcast");
+var citySearchInputEl = document.querySelector("#selectedCity");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveDayContainer");
+var pastSearchButtonEl = document.querySelector("#pastSearch");
+
