e1eab6a23d6024f99f49f9e7764b1b17b3a3e546
Charles Nyabeze <charlesnnyabeze@gmail.com>
2022-01-17 06:24:10 -0500

    future weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const formInput = document.querySelector('form')
 // Connect Connect current weather
 const todaysWeather = document.querySelector('#currentWeatherDiv')
 // Connect Future Weather
-const futureWeather = document.querySelector('#futureWeatherDiv')
+const futureWeather = document.querySelector('#futureForecast')
 // Connect API Key
 const myKey = '53b685361a5dbcb458e2d4f4d555cafc'
 // Connect City Name
@@ -52,9 +52,17 @@ function getWeather (query){
     .then(weather => {
         return weather.json();
     })
-    .then(displayResults);
+    .then(function(weather){
+        //Send data to display results
+        displayResults(weather)
+        // send data to display future results
+        displayFutureResults(weather)
+
+    })
+
 }
-// Insert Data to HTML
+
+// Insert Data from getWeather function to HTML
 function displayResults (weather){
     console.log(weather)
     //city name
@@ -75,8 +83,74 @@ function displayResults (weather){
     const icon = weather.weather[0].icon
     let locationIcon = document.querySelector('.weather-icon');
     locationIcon.innerHTML =`<img src="https://openweathermap.org/img/w/${icon}.png">`
-    //UV Index    
 }
+// Use data from getWeather
+function displayFutureResults(weather){
+    // Get longitude and latitude from getWeather API
+
+    let latitude = weather.coord.lat;
+    let longitude = weather.coord.lon;
+    //Get next 5 days of weather
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${myKey}`)
+    .then(weather => {
+        return weather.json();
+    })
+    //supply next function with weather data
+    .then(function(weather){
+        loadFutureReasults(weather)
+    })
+}
+//Insert Future Weather Data (next 5 days) into HTML
+// Fetch API for future weather
+// function loadFutureReasults(weather){
+//     console.log(weather)
+//     //temperature
+//     let temp = document.querySelector('.ftemp')
+//     temp.innerText = `Tempurature: ${weather.daily[0].temp.day}°C`
+//     //humidity
+//     let humidity = document.querySelector('.fhumidity')
+//     humidity.innerText = `Humidity: ${weather.daily[0].humidity}`
+//     //winds
+//     let wind = document.querySelector('.fwind')
+//     wind.innerText = `Wind Speed: ${weather.daily[0].wind_speed}km/h`
+//     //icon
+//     console.log(weather)
+//     const icon = weather.daily[0].weather[0].icon
+//     let locationIcon = document.querySelector('.fweather-icon');
+//     locationIcon.innerHTML =`<img src="https://openweathermap.org/img/w/${icon}.png">`
+
+// }
+
+function loadFutureReasults(weather) {
+    // Create divs if none
+    if ( $('#forecast').children().length == 0 ){
+      for (let i=0; i<5; i++){
+        let icon = `https://openweathermap.org/img/w/${weather.daily[i].weather[0].icon}.png`;
+        $('#forecast').append(`<div class='dailyForecast' id='forecast${i}'></div>`);
+        $(`#forecast${i}`).append(`<image class='forecastIcon' id='icon${i}' src='${icon}'></image>`);
+        $(`#forecast${i}`).append(`<p class='forecastText' id='temp${i}'>Temp: ${weather.daily[i].temp.day}°C</p>`);
+        $(`#forecast${i}`).append(`<p class='forecastText' id='wind${i}'>Wind: ${weather.daily[i].wind_speed}kph</p>`);
+        $(`#forecast${i}`).append(`<p class='forecastText' id='humid${i}'>Humidity: ${weather.daily[i].humidity}%</p>`);
+      };
+    }
+    //Rewrite if there are divs
+    else {
+        for (let i=0; i<5; i++){
+          let day = moment().add(i, 'days').format('L');
+          let icon = `https://openweathermap.org/img/w/${weather.daily[i].weather[0].icon}.png`;
+          document.getElementById(`date${i}`).innerHTML = day;
+          document.getElementById(`icon${i}`).src = icon;
+          document.getElementById(`temp${i}`).innerHTML = `Temp: ${weather.daily[i].temp.day}`;
+          document.getElementById(`wind${i}`).innerHTML = `Wind: ${weather.daily[i].wind_speed}`;
+          document.getElementById(`humid${i}`).innerHTML = `Humidity: ${weather.daily[i].humidity}`;
+        }
+      }
+}
+
+
+
+//UV Index
+
 
 // loop through past cities searched + add to list
 pastCities.forEach((city)=>{
@@ -91,26 +165,8 @@ clearHistory.addEventListener('click', function (){
     }
 })
 
-// Fetch API for future weather
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
 // To-Do:
-// Create logic to = certain colors to = certain numbers
+//UV Index - color that indicates whether the conditions are favorable, moderate, or severe
+// Create a way for when clicked search that term it searches
 
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-// To-Do:
-// Convert Future weather condition data to be able to put on page 
-// Icon - Temp - Humidity - Wind Speed - UV Index
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// To-Do:
-// Create a way for when clicked search that term
 
